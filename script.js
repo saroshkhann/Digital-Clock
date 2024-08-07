ca2cb5f3a7b21345dde0387b86a093e4466fc290
saroshkhann <ksarosh785@gmail.com>
2024-08-07 14:54:03 +0500

    Modifications

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const hourr = document.querySelector(".hour");
 const minn = document.querySelector(".min");
 const secc = document.querySelector(".sec");
 const pm = document.querySelector(".pm");
+const date = document.querySelector(".date");
+
+const day = new Date();
+const options = {
+  day: "numeric",
+  month: "long",
+  weekday: "long",
+  year: "numeric",
+};
+
+date.textContent = new Intl.DateTimeFormat("en-PK", options).format(day);
 
 const timer = setInterval(function () {
   const date = new Date();
@@ -11,7 +22,9 @@ const timer = setInterval(function () {
   const sec = date.getSeconds();
 
   hour = hour % 12 || 12; // Converts '0' to '12'
+
   const ampm = hour <= 12 ? "PM" : "AM";
+
   hourr.textContent = String(hour).padStart(2, 0);
   minn.textContent = String(min).padStart(2, 0);
   secc.textContent = String(sec).padStart(2, 0);
